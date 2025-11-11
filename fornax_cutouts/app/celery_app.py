04095aafc39002547b19a6a15eb8d29cbe7fa355
Daniel Sarmiento <dsarmiento@stsci.edu>
2025-11-11 13:52:59 -0500

    cursor: task logging

--- fornax_cutouts/app/celery_app.py
+++ fornax_cutouts/app/celery_app.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import ssl
+import sys
 
 from celery import Celery
 from celery.signals import worker_process_init, worker_process_shutdown
@@ -10,6 +11,7 @@ from redis import Redis, RedisCluster
 from fornax_cutouts.config import CONFIG
 from fornax_cutouts.jobs.redis import sync_redis_client_factory
 from fornax_cutouts.sources import cutout_registry
+from fornax_cutouts.utils.logging import StructuredJSONFormatter
 
 logger = get_task_logger("cutouts")
 
@@ -52,6 +54,37 @@ if CONFIG.redis.use_ssl:
     conf_update["redis_backend_use_ssl"] = {"ssl_cert_reqs": ssl.CERT_NONE}
 
 celery_app.conf.update(**conf_update)
+
+# Configure structured logging for Celery loggers
+if CONFIG.log.format == "json":
+    # Disable Celery's default logging setup to prevent interference
+    celery_app.conf.update(
+        worker_hijack_root_logger=False,
+        worker_log_format="",
+        worker_task_log_format="",
+    )
+    # Configure Celery loggers to use JSON formatter
+    log_level = CONFIG.log.level.upper()
+
+    # Create console handler with JSON formatter
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setLevel(log_level)
+    console_handler.setFormatter(StructuredJSONFormatter())
+
+    # Configure Celery loggers
+    for logger_name in ["celery", "celery.task", "celery.worker"]:
+        celery_logger = logging.getLogger(logger_name)
+        celery_logger.setLevel(log_level)
+        celery_logger.handlers.clear()
+        celery_logger.addHandler(console_handler)
+        celery_logger.propagate = False
+
+    # Configure the cutouts task logger
+    cutouts_logger = logging.getLogger("cutouts")
+    cutouts_logger.setLevel(log_level)
+    cutouts_logger.handlers.clear()
+    cutouts_logger.addHandler(console_handler)
+    cutouts_logger.propagate = False
 
 
 @worker_process_init.connect
