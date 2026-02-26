0a46a568989606170464c26df309bf00e39b037a
Daniel Sarmiento <dsarmiento@stsci.edu>
2026-02-25 21:10:07 -0500

    monkey patch astrocut

--- fornax_cutouts/app/celery_app.py
+++ fornax_cutouts/app/celery_app.py
@@ -76,9 +76,9 @@ def _monkey_patch_astrocut():
     """
     If block_size_mib is set, monkeypatch astrocut to pass block_size in fsspec_kwargs.
     """
-    block_size_mib = os.environ.get("S3FS_BLOCK_SIZE", "0.5")
-    block_size_mib = float(block_size_mib) if block_size_mib is not None else block_size_mib
-    if block_size_mib is None or block_size_mib <= 0:
+    block_size_mib = os.environ.get("S3FS_BLOCK_SIZE")
+    block_size_mib = float(block_size_mib) if block_size_mib is not None else 1.0
+    if block_size_mib <= 0:
         return
     block_size_bytes = int(block_size_mib * 1024 * 1024)
     try:
@@ -89,7 +89,7 @@ def _monkey_patch_astrocut():
         _orig_load = fits_cutout.FITSCutout._load_file_data
 
         def _patched_load(self, input_file):
-            fsspec_kwargs = {"anon": True, "default_block_size": block_size_bytes} if "s3://" in str(input_file) else None
+            fsspec_kwargs = {"anon": True, "default_block_size": block_size_bytes, "default_fill_cache": False} if "s3://" in str(input_file) else None
             hdulist = fits.open(input_file, mode="denywrite", memmap=True, fsspec_kwargs=fsspec_kwargs)
             infile_exts = np.where([hdu.is_image and hdu.size > 0 for hdu in hdulist])[0]
             cutout_inds = self._parse_extensions(input_file, infile_exts)
