32012f65048750d62f1e0ffd8c2a740384def9c3
Sarah Weissman <sweissman@stsci.edu>
2026-08-21 12:39:02 -0400

    add tests for api

---
+++ tests/api/test_api.py
@@ -0,0 +1,57 @@
+""" """
+
+import json
+from unittest import mock
+from unittest.mock import patch
+
+from fastapi.testclient import TestClient
+
+from fornax_cutouts.app.api import main_app
+from fornax_cutouts.models.base import TargetPosition
+from fornax_cutouts.models.cutouts import FilenameLookupResponse, FilenameWithMetadata
+from fornax_cutouts.models.metadata import FilenameRequest
+from fornax_cutouts.sources import AbstractMissionSource, MissionMetadata, cutout_registry
+
+
+# Register a fake source for testing
+@cutout_registry.register_source()
+class FakeSource(AbstractMissionSource):
+    metadata: MissionMetadata = MissionMetadata(
+        name="fake",
+        pixel_size=0.55,
+        max_cutout_size=4,
+        filter=["a", "b"],
+        survey=["c", "d"],
+    )
+
+    def get_filenames(self, **kwargs) -> list[FilenameLookupResponse]:
+        fake_position = TargetPosition(ra=210.8023, dec=54.34875)
+        file_with_metadata = FilenameWithMetadata(filename="example.fits", metadata={})
+        response = FilenameLookupResponse(mission="fake", target=fake_position, filenames=[file_with_metadata])
+        return [response]
+
+
+@patch("fornax_cutouts.routes.v1.cutouts.async_uws.AsyncRedisCutoutJob")
+def test_file_name_request_valid_across_apis(mock_job):
+    """Test that the mission parameters passed as a JSON string to the /async endpoint in async_uws.py is accepted
+    to the /filenames endpoint in metadata.py"""
+
+    # Mock the job creation for the async cutout endpoint
+    mock_instance = mock_job.return_value
+    mock_instance.create_job = mock.AsyncMock(return_value="job123")
+
+    # Set up the app for testing
+    client = TestClient(main_app)
+    file_name_request = FilenameRequest(survey=["c"], filter=["a"]).model_dump()
+    request_data = {"position": ["m101"], "mission": {"fake": file_name_request}}
+    response = client.post("/api/v0/filenames", json=request_data)
+    assert response.status_code == 200
+
+    # Check that we can send the same FilenameRequest to the async cutout endpoint
+    async_form_data = {"fake": json.dumps(file_name_request), "position": ["m101"], "size": 4}
+    response = client.post("api/v0/cutouts/async", data=async_form_data, follow_redirects=False)
+    assert response.status_code == 303
+
+    # Get the job id from the mock and check that we redirected to that location
+    job_id = mock_job.call_args.kwargs["job_id"]
+    assert response.headers["location"] == f"/api/v0/cutouts/async/{job_id}"
